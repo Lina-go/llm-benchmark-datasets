aff452333c0f939493f3e2b1819975bc94426273
Lina-go <l.gomez1@uniandes.edu.co>
2025-12-31 11:57:20 +0000

    changes

--- src/models/openai_llm.py
+++ src/models/openai_llm.py
@@ -15,7 +15,7 @@ class OpenAILLM(BaseLLM):
         
         self.client = AzureOpenAI(
             api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-            api_version="2024-10-01-preview",
+            api_version="2025-03-01-preview", # "2024-12-01-preview"
             azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
         )
     
